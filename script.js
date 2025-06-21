6562df6e9d8032ee7b7f9ffc528248a79368aa69
abkrym5555 <momo542004@gmail.com>
2025-06-21 20:07:14 +0300

    make a smooth scrool from nav

--- script.js
+++ script.js
@@ -5,8 +5,12 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+const section1 = document.querySelector('#section--1');
+const navLinkss = document.querySelector('.nav__links');
+
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -18,8 +22,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -27,5 +30,20 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
+  }
+});
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+navLinkss.addEventListener('click', function (e) {
+  e.preventDefault();
+  const targetElem = e.target;
+  const elmById = document.querySelector(targetElem.getAttribute('href'));
+  if (targetElem.classList.contains('nav__link')) {
+    elmById.scrollIntoView({ behavior: 'smooth' });
   }
 });

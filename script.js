e9532f8f2d59e4da7defe05b98ec289d5dfb049b
abkrym5555 <momo542004@gmail.com>
2025-06-21 23:29:50 +0300

    handling tabs buttons

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const section1 = document.querySelector('#section--1');
 const navLinkss = document.querySelector('.nav__links');
+const opTab = document.querySelectorAll('.operations__tab');
+const opTabContainer = document.querySelector('.operations__tab-container');
+const opTabContent = document.querySelectorAll('.operations__content');
 
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
@@ -46,4 +49,22 @@ navLinkss.addEventListener('click', function (e) {
   if (targetElem.classList.contains('nav__link')) {
     elmById.scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+opTabContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const activButt = e.target.closest('.operations__tab');
+
+  if (!activButt) return;
+  opTab.forEach(but => but.classList.remove('operations__tab--active'));
+  opTabContent.forEach(con =>
+    con.classList.remove('operations__content--active')
+  );
+
+  activButt.classList.add('operations__tab--active');
+  document
+    .querySelector(
+      `.operations__content--${activButt.getAttribute('data-tab')}`
+    )
+    .classList.add('operations__content--active');
 });
